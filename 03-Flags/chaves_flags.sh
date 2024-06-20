edf4809fcfd46492eba7459bc1a370f1fc09764a
mmortiz <marcelomaortiz@hotmail.com>
2024-06-20 10:18:07 -0300

    chaves_flags v1.1

--- 03-Flags/chaves_flags.sh
+++ 03-Flags/chaves_flags.sh
@@ -16,6 +16,9 @@
 # Histórico:
 #   v1.0 20/06/2024, Mateus:
 #       - Adicionado -s, -h & -v
+#   v1.1 20/06/2024, Mateus:
+#       - Adicionado -m
+#       - Refatorado os if's para o case
 # ------------------------------------------------------- #
 # Testado em:
 #   bash 5.2.15
@@ -25,7 +28,7 @@
 
 USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
 MENSAGEM_USO="
-    $0 - [OPÇÕES]
+    $(basename $0) - [OPÇÕES]
 
         -h - Menu de ajuda
         -v - Versão
@@ -36,16 +39,26 @@ VERSAO="v.1.0"
 
 # -----------------------  Execução  -------------------- #
 
-if [ "$1" = "-h" ]; then
-    echo "$MENSAGEM_USO" && exit 0
-fi
-if [ "$1" = "-v" ]; then
-    echo "$VERSAO" && exit 0
-fi
-if [ "$1" = "-s" ]; then
-    echo "$USUARIOS" | sort && exit 0
-fi
+# if [ "$1" = "-h" ]; then
+#     echo "$MENSAGEM_USO" && exit 0
+# fi
+# if [ "$1" = "-v" ]; then
+#     echo "$VERSAO" && exit 0
+# fi
+# if [ "$1" = "-s" ]; then
+#     echo "$USUARIOS" | sort && exit 0
+# fi
+# if [ "$1" = "-m" ]; then
+#     echo "$USUARIOS^^" && exit 0
+# fi
 
-echo "$USUARIOS"
+
+case "$1" in
+    -h) echo "$MENSAGEM_USO" && exit 0;;
+    -v) echo "$VERSAO" && exit 0;;
+    -s) echo "$USUARIOS" | sort && exit 0;;
+    -m) echo "${USUARIOS^^}" && exit 0;;
+    *) echo "$USUARIOS";;
+esac
 
 # ------------------------------------------------------- #
