28d3e3b823eb37a2156ac0df05ba9dab203df783
mmortiz <marcelomaortiz@hotmail.com>
2024-07-01 11:58:51 -0300

    versão 1.0 do extrai_titulos.sh

---
+++ 06-ExtraçãoDados/extrai_titulos.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# extrai_titulos.sh - Extrai titulos de uma paǵina web
+# Autor:        Mateus Muller
+#
+# ------------------------------------------------------- #
+# Script responsável por extrair determinado padrão de um
+# blog, Lxer, colocar em um arquivo e ler mostrando com
+# cores na tela
+#
+# Exemplos:
+#   $ ./chaves_flags.sh -s -m
+#   Neste exemplo o -s colocará em ordem alfabética, e o -m
+#   em maiúsculo
+# ------------------------------------------------------- #
+# Histórico:
+#   v1.0 01/07/2024, Mateus:
+#       - Expressão regular com fator em comum criada
+#   v1.1 01/07/2024, Mateus:
+#       - Criado primeiro código com cores
+
+# ------------------------------------------------------- #
+# Testado em:
+#   bash 5.2.15
+# ------------------------------------------------------- #
+
+# ------------------------  Testes  --------------------- #
+
+# Lynx instalado
+[ ! -x "$(which lynx)" ] && sudo apt install lynx -y 
+
+# -----------------------  Variáveis  ------------------- #
+
+PATH_SH=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+ARQUIVO_TITULOS="$PATH_SH/Files/titulos.txt"
+
+# -----------------------  Execução  -------------------- #
+
+lynx -source http://lxer.com/ | \
+    grep blurb | \
+    sed 's/<div.*line">//;s/<\/span.*meta">//;s/$/\n/' \
+    > $ARQUIVO_TITULOS
+
+# ------------------------------------------------------- #
